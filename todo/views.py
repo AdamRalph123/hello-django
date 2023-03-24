31024f682d243f3c3ab9393dce36a92c1f89401c
AdamRalph123 <adambyrne16@gmail.com>
2023-03-24 17:21:08 +0000

    Created an edit button

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
-from .forms import Itemform
+from .forms import ItemForm
 
 
 def get_todo_list(request):
@@ -8,18 +8,31 @@ def get_todo_list(request):
     context = {
         'items': items
     }
-    return render(request, 'todo/todo_list.html')
+    return render(request, 'todo/todo_list.html', context)
+
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        item.objects.create(name=name, done=done)
-
-        return redirect('get_todo_list')
-    form = Itemform()
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
     context = {
         'form': form
     }
     return render(request, 'todo/add_item.html', context)
-    
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
