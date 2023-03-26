12d4e92cdbaf98a1a7a6acb5baa830f0a1b898e0
AdamRalph123 <adambyrne16@gmail.com>
2023-03-26 16:18:36 +0000

    Prepared to deploy to heroku

--- env.py
+++ env.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 import os
+import dj_database_url
+import env
 
 os.environ["VARIABLE_NAME"] = "variable value"
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+os.environ.setdefault("DATABASE_URL", postgres://lxuyofcz:C_sCQ2wyUEoNH_mcbJ1QKqDTH9Y42bJp@mel.db.elephantsql.com/lxuyofcz)
